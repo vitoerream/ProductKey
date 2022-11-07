5e019f5cd183eff647cef47976144036053f9383
vitoerream <117685977+vitoerream@users.noreply.github.com>
2022-11-07 22:07:18 +0100

    Create productkeywiewer.bat

---
+++ productkeywiewer.bat
@@ -0,0 +1,6 @@
+@echo off
+echo @vitoerream 07/11/2022
+echo Codice Product Key Windows, non mostrarlo a nessuno!
+pause
+Reg Query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" /v BackupProductKeyDefault
+pause
